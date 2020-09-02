b65fb61cf669bb7e9cdd59b088f146f523cdfb9e
guangshinj <476506867@qq.com>
2020-09-02 23:41:13 +0800

    frame structure

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-import _ from 'lodash'; // 第三方工具库
-import { add, sub } from 'ts-loader!./utils/calc.ts' // 内联调用ts-loader处理ts源代码
-import './css/index.css'
 
-console.info('begin')
-function component() {
-  var element = document.createElement('div');
+import './typescript/index'
+import './css/index'
+import './js-dom/index'
 
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  return element;
-}
-
-document.body.appendChild(component());
-console.info(add(1, 2))
--- src/utils/calc.ts
+++
@@ -1,9 +0,0 @@
-function add(a: number, b: number): number {
-  return a + b;
-}
-
-function sub(a: number, b: number): number {
-  return a - b;
-}
-
-export { add, sub };
